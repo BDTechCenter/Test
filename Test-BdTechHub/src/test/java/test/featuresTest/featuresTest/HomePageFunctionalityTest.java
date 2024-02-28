f252dc45379220be0c4ed1ff92d56cd90d602ff2
joaop-ribeiro <joao06.ribeiro.costa@gmail.com>
2024-02-28 08:34:46 -0300

    feat: test notFindingTheReadMoreButtonWhenItHasTheNotNewsError

--- Test-BdTechHub/src/test/java/test/featuresTest/featuresTest/HomePageFunctionalityTest.java
+++ Test-BdTechHub/src/test/java/test/featuresTest/featuresTest/HomePageFunctionalityTest.java
@@ -3,10 +3,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import test.featuresTest.configurations.Configurations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class HomePageFunctionalityTest extends Configurations {
 
@@ -17,7 +20,7 @@ public class HomePageFunctionalityTest extends Configurations {
 
 
     @Test
-    public void ErrorNotFindingNews(){
+    public void errorNotFindingNews(){
         //TEST OBJECTIVE
         //{
         // This test should be run without the API information to ensure the error message
@@ -36,6 +39,30 @@ public class HomePageFunctionalityTest extends Configurations {
             // Compare the variable 'messageError' error message with the 'text' value of the 'textError' element
             assertEquals(messageError, textError.getText());
         //};
+    }
+
+    @Test
+    public void notFindingTheReadMoreButtonWhenItHasTheNotNewsError(){
+        //TEST OBJECTIVE
+        //{
+        // This test aims to verify that when the 'Not new' error occurs,
+        // it is not possible to view or interact with the 'Read more' button
+        //};
+
+        // TEST SCRIPT
+        //{
+        try {
+            // try to identify the 'button' of the 'text':'Read More'
+            WebElement buttonReadMore = driver.findElement(By.xpath("//a/button[text()=\"Read More\"]"));
+            // try to 'click' in 'buttonReadMore'
+            buttonReadMore.click();
+            // if the 'button': 'Read More' is found the test should fail
+            fail();
+        } catch (NoSuchElementException e){
+            // if the 'button': 'Read More' is not found, the test must pass
+        }
+        //};
+
     }
 
     @AfterEach
